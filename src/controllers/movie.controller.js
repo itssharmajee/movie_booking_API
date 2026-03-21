2fe4854ee4dca453e1393907059eb1998a8adc8e
itssharmajee <sgautamkumar71@gmail.com>
2026-03-21 17:51:26 +0530

    added feature for get paricular movie and also delete a movie

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -45,3 +45,62 @@ export const createMovie = async (req, res) => {
 
     }
 }
+
+export const deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findByIdAndDelete(id);
+        if (!movie) {
+            return res.status(404).json({
+                success: false,
+                message: "Movie does not exist",
+                error: {}
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            error: {},
+            message: "Successfully deleted movie",
+            data: movie
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error: err,
+            data: {}
+        })
+
+    }
+}
+
+export const getMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findOne({ _id: id });
+
+
+        if (!movie) {
+            return res.status(404).json({
+                success: false,
+                message: "Movie does not exists",
+                error: {}
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "data fetched successfully",
+            data: movie,
+            error: {}
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            data: {},
+            error: err
+        })
+    }
+}
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { createMovie } from "../controllers/movie.controller.js";
+import { createMovie, deleteMovie, getMovie } from "../controllers/movie.controller.js";
 
 export const movieRoutes = express.Router();
 
 movieRoutes.post("/",createMovie);
+movieRoutes.get("/:id",getMovie);
+movieRoutes.delete("/:id",deleteMovie);
